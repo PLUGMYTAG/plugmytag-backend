9d374ec8ac8d29c3b089752fc0e462fb618b0208
PLUGMYTAG <plugmytag@gmail.com>
2025-06-19 23:46:58 +0200

    Create plugmytag_10_tag_test_config.js

---
+++ utils/data/plugmytag_10_tag_test_config.js
@@ -0,0 +1,13 @@
+[
+  { "voice_id": "c8PX8tI3Cx46TLRopRq8" },
+  { "voice_id": "TQ1YahPcaDMsoIPw34oE" },
+  { "voice_id": "03vEurziQfq3V8WZhQvn" },
+  { "voice_id": "Ybqj6CIlqb6M85s9Bl4n" },
+  { "voice_id": "CVRACyqNcQefTlxMj9bt" },
+  { "voice_id": "dPah2VEoifKnZT37774q" },
+  { "voice_id": "BpjGufoPiobT79j2vtj4" },
+  { "voice_id": "RPdRfxxQOaNxn1LtRQqm" },
+  { "voice_id": "gLqGVg4EE30kJbMp6nxl" },
+  { "voice_id": "3uGTjgQn1UwfzgY0VkZG" },
+  { "voice_id": "D11AWvkESE7DJwqIVi7L" }
+]
